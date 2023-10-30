a4606f2afa7cc7e782813a1d9978743bfe1f266a
DanyaLyubimov <danya.lubimov@yandex.ru>
2023-10-30 23:16:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dz1/MainActivity.kt
@@ -0,0 +1,167 @@
+package com.example.dz1
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CreateSquares()
+            // A surface container using the 'background' color from the theme
+
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun CreateSquares() {
+    var remN by rememberSaveable {
+        mutableStateOf(0)
+    }
+    val inLineN: Int
+    val configuration = LocalConfiguration.current
+    inLineN = when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            4
+        }
+        else -> {
+            3
+        }
+    }
+
+    Surface(
+        modifier = Modifier
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.LightGray)
+                .padding(10.dp)
+        ) {
+
+
+            Column(
+                modifier = Modifier
+                    .weight(4f)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                for (i in 0 until remN / inLineN) {
+                    Row(modifier = Modifier, horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                        for (j in 1..inLineN) {
+                            Box(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .aspectRatio(1f)
+                                    .background(if ((i * inLineN + j) % 2 == 0) Color.Red else Color.Blue)
+                            ) {
+                                Text(text = "${i * inLineN + j}")
+                            }
+                        }
+                    }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+
+                    for (j in 1..remN % inLineN) {
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .aspectRatio(1f)
+                                .background(if ((remN - remN % inLineN + j) % 2 == 0) Color.Red else Color.Blue)
+                        ) {
+                            Text(text = "${remN - remN % inLineN + j}")
+                        }
+                    }
+                    repeat(inLineN - remN % inLineN) {
+                        Spacer(
+                            modifier = Modifier
+                                .weight(1f)
+                        )
+                    }
+                }
+            }
+            Column(modifier = Modifier.weight(1f), verticalArrangement = Arrangement.Bottom) {
+
+
+                Text(text = "$remN")
+
+                Box(modifier = Modifier
+                    .clickable {
+                        remN++
+                    }
+                    .size(50.dp)
+                    .background(Color.Blue)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = "Добавить квадрат",
+                        modifier = Modifier.size(50.dp)
+                    )
+                }
+            }
+        }
+
+
+    }
+}
+
+
